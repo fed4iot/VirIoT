e840e28dae1ef9ea9241e4783d74809e06249a56
Giuseppe Tropea <giutropea@gmail.com>
2020-02-28 10:59:41 +0100

    added empty data folder to master controller

---
+++ Master-Controller/data/settings-docker-template.py
@@ -0,0 +1,17 @@
+# public IP address through the which it is possible to access thingvisors, database, vSilos, etc.
+default_gateway_IP = ""
+
+# MQTT settings (possibly two brokers one for control and another one for control messages)
+MQTT_data_broker_IP = "172.17.0.1"
+MQTT_data_broker_port = 1883
+MQTT_control_broker_IP = "172.17.0.1"
+MQTT_control_broker_port = 1883
+
+# Mongo settings
+# mongoDb port
+mongo_port = 27017
+
+container_manager = "DOCKER"
+
+#Authentication settings
+JWT_SECRET_KEY = "UYG867ti867f(/&$SWRUco)(YPO/T"  # secret key used to encrypt password

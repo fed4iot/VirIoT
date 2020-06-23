36f3ec892e1969f0a0406691969093ef8ef186ba
houcem.kacem <houcem.kacem@eglobalmark.com>
2020-06-23 16:01:41 +0200

    feat: prepare stellio to be deployed as vSilo

---
+++ Flavours/stellio-flavour/start.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+/etc/init.d/postgresql start
+exec >/dev/tty 2>/dev/tty </dev/tty && screen -S zookeeper -s /bin/bash -t win0 -A -d -m
+exec >/dev/tty 2>/dev/tty </dev/tty && screen -S kafka -s /bin/bash -t win0 -A -d -m
+exec >/dev/tty 2>/dev/tty </dev/tty && screen -S neo4j -s /bin/bash -t win0 -A -d -m
+exec >/dev/tty 2>/dev/tty </dev/tty && screen -S api-gateway -s /bin/bash -t win0 -A -d -m
+exec >/dev/tty 2>/dev/tty </dev/tty && screen -S entity-service -s /bin/bash -t win0 -A -d -m
+exec >/dev/tty 2>/dev/tty </dev/tty && screen -S search-service -s /bin/bash -t win0 -A -d -m
+exec >/dev/tty 2>/dev/tty </dev/tty && screen -S subscription-service -s /bin/bash -t win0 -A -d -m
+
+echo "Starting zookeeper"
+screen -S zookeeper -p win0 -X stuff $'kafka_2.12-2.3.0/bin/zookeeper-server-start.sh kafka_2.12-2.3.0/config/zookeeper.properties\n'
+sleep 20
+echo "Zookeeper started"
+echo "Starting kafka"
+screen -S kafka -p win0 -X stuff $'kafka_2.12-2.3.0/bin/kafka-server-start.sh kafka_2.12-2.3.0/config/server.properties\n'
+sleep 20
+echo "Kafka started"
+echo "Starting neo4j"
+screen -S neo4j -p win0 -X stuff $'service neo4j start\n'
+sleep 20
+echo "Neo4j started"
+echo "Starting gateway"
+screen -S api-gateway -p win0 -X stuff $'java -Xms512m -jar stellio/api-gateway-0.5.0.jar\n'
+sleep 30
+echo "Gateway started"
+echo "Starting search service"
+screen -S search-service -p win0 -X stuff $'java -Xms512m -jar stellio/search-service-0.5.0.jar\n'
+sleep 30
+echo "Search service started"
+echo "Starting subscription service"
+screen -S subscription-service -p win0 -X stuff $'java -Xms512m -jar stellio/subscription-service-0.5.0.jar\n'
+sleep 30
+echo "Subscription service started"
+echo "Starting entity service"
+screen -S entity-service -p win0 -X stuff $'java -Xms512m -jar stellio/entity-service-0.5.0.jar\n'
+sleep 30
+echo "Entity service started"
+sleep infinity

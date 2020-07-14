bb566149bae25eb377cdcb1200e2046cb5dbfeb0
andrea.detti <andrea.detti@uniroma2.it>
2020-07-14 10:29:11 +0200

    remove stale actuator

--- Flavours/mobius-pub-sub-flavour/mobius_setup.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-rm conf.json
-
-echo '{"csebaseport":"7579","dbpass":"fed4iot"}' > conf.json
-
-sed -i 's/Mobius2/Mobius/g' mobius.js
-
-sed -i '21s/^/CREATE DATABASE IF NOT EXISTS mobiusdb;\nUSE mobiusdb\n/' mobius/mobiusdb.sql

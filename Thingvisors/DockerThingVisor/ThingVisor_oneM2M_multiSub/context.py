d1a3a444101c772fdd752dc2b7a91e327d965791
Vincenzo Failla <giutropea@gmail.com>
2020-11-02 13:23:49 +0100

    fixed bug in context.py

--- Thingvisors/DockerThingVisor/ThingVisor_oneM2M_multiSub/context.py
+++ Thingvisors/DockerThingVisor/ThingVisor_oneM2M_multiSub/context.py
@@ -8,13 +8,21 @@ class Context:
         return self.contextMap
 
     def update(self, entities):
-        for new_entity in entities:
-            for old_entity in self.contextMap:
-                if old_entity["id"] == new_entity["id"]:
-                    self.contextMap.remove(old_entity)
-                    self.contextMap.append(jsonmerge.merge(old_entity,new_entity))
-                    break
-                else:
+        if len(self.contextMap) == 0:
+            self.set_all(entities)
+        else:
+            for new_entity in entities:
+                not_found = True
+                for old_entity in self.contextMap:
+                    if old_entity["id"] == new_entity["id"]:
+                        self.contextMap.remove(old_entity)
+                        try:
+                            self.contextMap.append(jsonmerge.merge(old_entity,new_entity))
+                        except:
+                            self.contextMap.append(new_entity)
+                        not_found = False
+                        break
+                if not_found == True:
                     self.contextMap.append(new_entity)
 
     def set_all(self, entities):

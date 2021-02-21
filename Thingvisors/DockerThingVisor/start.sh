ac30790007aea49d5f705078bc9bdba37bd757f9
MukaiLab <38516309+MukaiLab@users.noreply.github.com>
2021-02-21 23:32:59 +0900

    Delete start.sh

--- Thingvisors/DockerThingVisor/start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-/usr/bin/screen -S hypervisor -s /bin/bash -t win0 -A -d -m
-screen -S hypervisor -p win0 -X stuff $'/usr/local/bin/python3 /app/thingVisor_wildlifemonitoring_ver1_0.py \n'
-
-sleep infinity
